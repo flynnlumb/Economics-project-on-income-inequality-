83be81d8f70e74748c8a5e556401c1ecd01bb91d
Flynn Lumb <flynnlumb@gmail.com>
2026-08-12 13:22:56 +0100

    Added cleaned world bank data set on percentage change in RGDP/capita per year

--- python/Income_inequality_growth/analysis.py
+++ python/Income_inequality_growth/analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests 
 from pathlib import Path
-url = "https://api.worldbank.org/v2/country/all/indicator/SI.POV.GINI?format=json&per_page=20000"
+url = "https://api.worldbank.org/v2/country/all/indicator/NY.GDP.PCAP.KD.ZG?format=json&per_page=20000"
 response = requests.get(url)
 data =response.json()[1]
 df = pd.DataFrame(data)
@@ -15,4 +15,4 @@ print(df['date'].min(), df['date'].max())
 
 data_folder =Path(__file__).parent / "data"
 data_folder.mkdir(exist_ok=True)
-df.to_csv(data_folder / "gini_data.csv", index=False)
+df.to_csv(data_folder / "growth_data.csv", index=False)
